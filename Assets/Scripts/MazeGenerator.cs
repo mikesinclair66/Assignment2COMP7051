0915c6a5f1acf28f5f7007810aa6cc51aec59557
Michael Sinclair <michaelkevin.sinclair@gmail.com>
2021-11-04 19:41:50 -0700

    Added maze generator script

---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class MazeSector
+{
+    /*
+     * 0 - 4 walls
+     * 1 - 3 walls (blank at top)
+     * 2 - 3 walls (blank at left)
+     * 3 - 2 walls (blank at top and left)
+     */
+}
+
+public class MazeGenerator : MonoBehaviour
+{
+    const int SIZE = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
