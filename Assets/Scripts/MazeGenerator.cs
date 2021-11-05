091c4dcc6828cbedf2eae99fb754c4763926c7e3
Michael Sinclair <michaelkevin.sinclair@gmail.com>
2021-11-05 12:50:51 -0700

    Finished grid system

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -9,15 +9,17 @@ class MazeSector
      * type indicates the walls of the sector, respectively starting from the top
      * and going clockwise.
      * 0 - 4 walls
-     * 1 - 3 walls (blank at top)
+     * 1 - 3 walls (blank at bottom)
      * 2 - 3 walls (blank at left)
-     * 3 - 2 walls (blank at top and left)
+     * 3 - 2 walls (blank at bottom and left)
      */
     int type;
     bool[] hidden;
     GameObject[] quads;
+    GameObject[] mirrorQuads;//quads of the same position facing the other way
 
     static float sectorSize;
+    static int mazeSize;
 
     public MazeSector(int type, GameObject quad,
         float xOffset, float yOffset)
@@ -44,6 +46,8 @@ class MazeSector
 
         hidden = new bool[quadAmount];
         quads = new GameObject[quadAmount];
+        mirrorQuads = new GameObject[quadAmount];
+        float startX = ((mazeSize % 2 == 0) ? sectorSize / 2 : 0);
         for(int i = 0; i < quadAmount; i++)
         {
             hidden[i] = false;
@@ -51,7 +55,105 @@ class MazeSector
             quads[i].gameObject.transform.localScale = new Vector3(sectorSize,
                 quad.gameObject.transform.localScale.y, 1);
             quads[i].gameObject.transform.position = quads[i].gameObject.transform.position +
-                new Vector3(sectorSize / 2 + xOffset * sectorSize, 0, sectorSize / 2 + yOffset * sectorSize);
+                new Vector3(startX + xOffset * sectorSize, 0, startX + yOffset * sectorSize);
+
+            mirrorQuads[i] = GameObject.Instantiate(quad, quad.gameObject.transform.position, quad.gameObject.transform.rotation);
+            mirrorQuads[i].gameObject.transform.localScale = new Vector3(sectorSize,
+                quad.gameObject.transform.localScale.y, 1);
+            //leave mirrorQuads positioning for the following
+        }
+
+        //position the walls relative to its sector point
+        if(type == 0)
+        {
+            quads[0].gameObject.transform.position = quads[0].gameObject.transform.position +
+                new Vector3(0, 0, sectorSize / 2);
+            quads[1].gameObject.transform.Rotate(0, -90, 0);
+            quads[1].gameObject.transform.position = quads[1].gameObject.transform.position +
+                new Vector3(sectorSize / 2, 0, 0);
+            quads[2].gameObject.transform.position = quads[2].gameObject.transform.position +
+                new Vector3(0, 0, -sectorSize / 2);
+            quads[3].gameObject.transform.Rotate(0, -90, 0);
+            quads[3].gameObject.transform.position = quads[3].gameObject.transform.position +
+                new Vector3(-sectorSize / 2, 0, 0);
+
+            mirrorQuads[0].gameObject.transform.Rotate(0, 0, 180);
+            mirrorQuads[1].gameObject.transform.Rotate(0, 90, 0);
+            mirrorQuads[2].gameObject.transform.Rotate(0, 0, 180);
+            mirrorQuads[3].gameObject.transform.Rotate(0, 90, 0);
+        }
+        else if(type == 1)
+        {
+            quads[0].gameObject.transform.position = quads[0].gameObject.transform.position +
+                new Vector3(0, 0, sectorSize / 2);
+            quads[1].gameObject.transform.Rotate(0, -90, 0);
+            quads[1].gameObject.transform.position = quads[1].gameObject.transform.position +
+                new Vector3(sectorSize / 2, 0, 0);
+            quads[2].gameObject.transform.Rotate(0, -90, 0);
+            quads[2].gameObject.transform.position = quads[2].gameObject.transform.position +
+                new Vector3(-sectorSize / 2, 0, 0);
+
+            mirrorQuads[0].gameObject.transform.Rotate(0, 0, 180);
+            mirrorQuads[1].gameObject.transform.Rotate(0, 90, 0);
+            mirrorQuads[2].gameObject.transform.Rotate(0, 90, 0);
+        }
+        else if(type == 2)
+        {
+            quads[0].gameObject.transform.position = quads[0].gameObject.transform.position +
+                new Vector3(0, 0, sectorSize / 2);
+            quads[1].gameObject.transform.Rotate(0, -90, 0);
+            quads[1].gameObject.transform.position = quads[1].gameObject.transform.position +
+                new Vector3(sectorSize / 2, 0, 0);
+            quads[2].gameObject.transform.position = quads[2].gameObject.transform.position +
+                new Vector3(0, 0, -sectorSize / 2);
+
+            mirrorQuads[0].gameObject.transform.Rotate(0, 0, 180);
+            mirrorQuads[1].gameObject.transform.Rotate(0, 90, 0);
+            mirrorQuads[2].gameObject.transform.Rotate(0, 0, 180);
+        }
+        else
+        {
+            quads[0].gameObject.transform.position = quads[0].gameObject.transform.position +
+                new Vector3(0, 0, sectorSize / 2);
+            quads[1].gameObject.transform.Rotate(0, -90, 0);
+            quads[1].gameObject.transform.position = quads[1].gameObject.transform.position +
+                new Vector3(sectorSize / 2, 0, 0);
+
+            mirrorQuads[0].gameObject.transform.Rotate(0, 0, 180);
+            mirrorQuads[1].gameObject.transform.Rotate(0, 90, 0);
+        }
+
+        for (int i = 0; i < quadAmount; i++)
+            mirrorQuads[i].gameObject.transform.position = quads[i].gameObject.transform.position;
+    }
+
+    /// <summary>
+    /// Sets whether the specifie wall is hidden based on the sector type.
+    /// </summary>
+    /// <param name="index"></param>
+    public void SetWallHidden(int index, bool isHidden)
+    {
+        string err = "Index is out of range for the respective sector type.";
+        hidden[index] = isHidden;
+        quads[index].SetActive(!isHidden);
+        mirrorQuads[index].SetActive(!isHidden);
+
+        switch (type)
+        {
+            case 0:
+                if (index < 0 || index > 3)
+                    throw new ArgumentException(err);
+                break;
+            case 1:
+            case 2:
+                if(index < 0 || index > 2)
+                    throw new ArgumentException(err);
+                break;
+            case 3:
+            default:
+                if(index < 0 || index > 1)
+                    throw new ArgumentException(err);
+                break;
         }
     }
 
@@ -64,11 +166,16 @@ class MazeSector
         MazeSector.sectorSize = sectorSize;
         //Debug.Log("Sector size: " + sectorSize);
     }
+
+    public static void SetMazeSize(int mazeSize)
+    {
+        MazeSector.mazeSize = mazeSize;
+    }
 }
 
 public class MazeGenerator : MonoBehaviour
 {
-    const int SIZE = 8;
+    const int SIZE = 6;//the area of the grid (SIZE X SIZE)
     MazeSector[,] sectors = new MazeSector[SIZE, SIZE];
     GameObject plane, standardWall;
 
@@ -78,8 +185,10 @@ public class MazeGenerator : MonoBehaviour
         plane = GameObject.Find("Plane");
         standardWall = GameObject.Find("Plane/StandardWall");
         MazeSector.SetSectorSize(10.0f * plane.gameObject.transform.localScale.x / (float)SIZE);
+        MazeSector.SetMazeSize(SIZE);
         InitSectors();
         standardWall.SetActive(false);
+        GeneratePath();
     }
 
     // Update is called once per frame
@@ -108,5 +217,11 @@ public class MazeGenerator : MonoBehaviour
                     c - SIZE / 2, r - SIZE / 2);
             }
         }
+    }
+
+    void GeneratePath()
+    {
+        sectors[0, 0].SetWallHidden(3, true);
+        sectors[SIZE - 1, SIZE - 1].SetWallHidden(1, true);
     }
 }
