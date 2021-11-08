993d9af91a6d773db63e2f5b97bb67efceab022c
Michael Sinclair <michaelkevin.sinclair@gmail.com>
2021-11-07 21:28:45 -0800

    Almost got enemy ai working

--- Assets/Scripts/DudeEnemy.cs
+++ Assets/Scripts/DudeEnemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,38 +7,161 @@ using UnityEngine.AI;
 public class DudeEnemy : MonoBehaviour
 {
     public GameObject Player;
-    public float Distance;
-
-    public bool isAngered;
-
     public NavMeshAgent _agent;
+    public GameObject plane;
+    GameObject enemy;
+    MazeGenerator mazeGenerator;
+    Vector3 startPos;
+    int SIZE;
+    int prevX, prevY;
+    int x, y;
+    float distX, distY;
+    float planeScale;
+    int direction = 0;
+    const float SPEED = 2.25f;
+    bool paused = true;
+
+    void Start()
+    {
+        enemy = GameObject.Find("Enemy");
+        _agent.isStopped = false;
+        planeScale = plane.transform.localScale.x;
+        mazeGenerator = plane.GetComponent<MazeGenerator>();
+        startPos = mazeGenerator.GetStartLocation();
+        enemy.transform.position = startPos;
+        SIZE = mazeGenerator.GetSize();
+        x = SIZE - 1;
+        y = x;
+        prevX = x;
+        prevY = y;
+        distX = 0;
+        distY = 0;
+    }
+
+    public void Init()
+    {
+        ChangeDirection(3);
+        if (!mazeGenerator.IsSectorHidden(y, x - 1, 1))
+            ChangeDirection(2);
+        Animator anim = GetComponent<Animator>();
+        anim.enabled = true;
+        anim.Play("Dude Walk");
+        paused = false;
+    }
 
     void Update()
     {
-        Distance = Vector3.Distance(Player.transform.position, this.transform.position);
-        Animator anim = GetComponent<Animator>();
+        if (!paused)
+        {
+            switch (direction)
+            {
+                case 0:
+                    distY += SPEED * Time.deltaTime;
+                    break;
+                case 1:
+                    distX += SPEED * Time.deltaTime;
+                    break;
+                case 2:
+                    distY -= SPEED * Time.deltaTime;
+                    break;
+                case 3:
+                default:
+                    distX -= SPEED * Time.deltaTime;
+                    break;
+            }
+            enemy.transform.position = startPos
+                + new Vector3(distX, 0, distY);
 
-        if (Distance <= 50)
-        {
-            isAngered = true;
+            for (int d = 0; d < SIZE - 1; d++)
+            {
+                if ((float)d * 30.0f / (float)SIZE <= Math.Abs(distX)
+                    && (float)(d + 1) * 30.0f / (float)SIZE > Math.Abs(distX))
+                {
+                    x = SIZE - d - 1;
+                }
+            }
+
+            for (int d = 0; d < SIZE - 1; d++)
+            {
+                if ((float)d * 30.0f / (float)SIZE <= Math.Abs(distY)
+                    && (float)(d + 1) * 30.0f / (float)SIZE > Math.Abs(distY))
+                {
+                    y = SIZE - d - 1;
+                }
+            }
+
+            if ((x != prevX && distX < -(SIZE - x - 1) * 30.0f / (float)SIZE/* - 15.0f / (float)SIZE*/)
+                || (y != prevY && distY < -(SIZE - y - 1) * 30.0f / (float)SIZE/* - 15.0f / (float)SIZE*/))
+            {
+                prevX = x;
+                prevY = y;
+                FindRandomDirection();
+            }
         }
-        if (Distance > 50f)
+    }
+
+    void FindRandomDirection()
+    {
+        bool[] valid = { false, false, false, false };
+
+        System.Random ran = new System.Random();
+        //Debug.Log("Valid: {" + valid[0] + ", " + valid[1] + ", " + valid[2] + ", " + valid[3]);
+        Debug.Log("Turn: X: " + x + ", Y: " + y);
+        int validTurn = -1;
+        paused = true;
+        while(validTurn == -1)
         {
-            isAngered = false;
+            validTurn = ran.Next(0, 4);
+
+            if (y < SIZE)
+            {
+                valid[0] = mazeGenerator.IsSectorHidden(y, x, 0);
+            }
+            if (y - 1 >= 0)
+            {
+                valid[2] = mazeGenerator.IsSectorHidden(y - 1, x, 0);
+            }
+
+            if (x < SIZE)
+            {
+                valid[1] = mazeGenerator.IsSectorHidden(y, x, 1);
+            }
+            if (x - 1 >= 0)
+            {
+                valid[3] = mazeGenerator.IsSectorHidden(y, x - 1, 1);
+            }
+
+            if (!valid[validTurn]
+                /*|| (validTurn == 0 && direction == 2 && valid[2])
+                || (validTurn == 1 && direction == 3 && valid[3])
+                || (validTurn == 2 && direction == 0 && valid[0])
+                || (validTurn == 3 && direction == 1 && valid[1])*/)
+                validTurn = -1;
         }
 
-        if (isAngered)
+        ChangeDirection(validTurn);
+        paused = false;
+    }
+
+    void ChangeDirection(int dir)
+    {
+        direction = dir;
+
+        switch (dir)
         {
-            anim.enabled = true;
-            anim.Play("Dude Walk");
-            _agent.isStopped = false;
-            _agent.SetDestination(Player.transform.position);
-        }
-        if (!isAngered)
-        {
-            anim.enabled = false;
-            anim.StopPlayback();
-            _agent.isStopped = true;
+            case 0:
+                enemy.transform.rotation = Quaternion.Euler(0, 0, 0);
+                break;
+            case 1:
+                enemy.transform.rotation = Quaternion.Euler(0, 90, 0);
+                break;
+            case 2:
+                enemy.transform.rotation = Quaternion.Euler(0, 180, 0);
+                break;
+            case 3:
+            default:
+                enemy.transform.rotation = Quaternion.Euler(0, 270, 0);
+                break;
         }
     }
 }
